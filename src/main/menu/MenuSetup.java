2fddcf5ff6f1dc52e9f240c15f1dea3034d8143d
Minh Chau <lilichi4423@gmail.com>
2022-10-04 09:48:43 +0700

    Update 4/10/2022

--- src/main/menu/MenuSetup.java
+++ src/main/menu/MenuSetup.java
@@ -13,7 +13,7 @@ public class MenuSetup {
     public MenuBar menuBar = new MenuBar();
 
     public Menu fileMenu = new Menu("File");
-    public MenuItem fileMenu1 = new MenuItem("new");
+    public MenuItem fileMenu1 = new MenuItem("New");
     public MenuSelectionManager menuSelectionManager = new MenuSelectionManager();
     public MenuItem fileMenu2 = new MenuItem("Pause");
     public MenuItem fileMenu3 = new MenuItem("Exit");
@@ -28,7 +28,16 @@ public class MenuSetup {
     public void addMenu() {
         fileMenu.getItems().addAll(fileMenu1, fileMenu2, fileMenu3);
         helpMenu1.setOnAction(e -> {
-            System.out.println("Help");
+            System.out.println("Instruction");
+        });
+        fileMenu1.setOnAction(e-> {
+            System.out.println("New");
+        });
+        fileMenu2.setOnAction(e-> {
+            System.out.println("Pause");
+        });
+        fileMenu3.setOnAction(e-> {
+            System.out.println("Exit");
         });
         HelpMenu.getItems().addAll(helpMenu1);
         menuBar.getMenus().addAll(fileMenu, HelpMenu);
